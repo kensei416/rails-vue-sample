50876e12bebf7ebff33117ebe6efea7e4eaa131d
Kensei itatani <kensei.itatani@mulodo.com>
2017-12-20 15:13:26 +0700

    Create User test #1

--- db/migrate/20171219073044_create_tasks.rb
+++ db/migrate/20171219073044_create_tasks.rb
@@ -1,7 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
+      t.string :user_id
       t.string :title, null: false
+      t.string :category
       t.boolean :is_done, default: false, null: false
       t.boolean :fav, default: false, null: false
 
