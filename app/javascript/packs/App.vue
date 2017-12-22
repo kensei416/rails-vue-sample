3be246b1774ec77c129a206d85eb9e6f3803a30a
Kensei itatani <kensei.itatani@mulodo.com>
2017-12-22 17:22:55 +0700

    Fix header size #3

---
+++ app/javascript/packs/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <headerrr/>
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Login from './components/login.vue'
+
+  export default {
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+    methods: {
+      navigateTo (root) {
+        this.$router.push(root)
+      }
+    },
+    computed:{
+      logged_in() {
+        return this.$store.getters.isUserLoggedIn
+      }
+    },
+    components: {
+    'login': Login,
+    'headerrr': Header
+    }
+  }
+</script>
+
+<style>
+.dialog {
+  background-color: black;
+}
+</style>
