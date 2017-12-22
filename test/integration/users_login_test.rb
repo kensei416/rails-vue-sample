7403e7239f8a51121e74f420c8a563c6d81c51c4
Kensei itatani <kensei.itatani@mulodo.com>
2017-12-22 09:40:55 +0700

    Add login test after logout #1

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  test "login with invalid information" do
+    get login_path
+    post api_sessions_path, params: {session: {email: "", user_id: "", password: ""}}
+    assert_not is_logged_in?
+  end
+
   test "login with valid information" do
     get login_path
     post api_sessions_path, params: { session: 
@@ -15,4 +21,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
                           }}
     assert is_logged_in?                          
   end  
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post api_sessions_path, params: { session: 
+                          {
+                            email: @user.email, 
+                            user_id: @user.user_id,
+                            password: 'password'
+                          }}
+    assert is_logged_in?   
+    delete api_session_path(@user)
+    assert_not is_logged_in?
+  end
 end
