d9c4682dce5ff082d7314bf44d1843e3232d4364
kensei416 <kensei416@gmail.com>
2017-12-24 18:53:17 +0700

    Revert "Fix user"

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const UglifyPlugin = require('uglifyjs-webpack-plugin')
 const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
-environment.plugins.set('UglifyJs', new UglifyPlugin())
 module.exports = environment
